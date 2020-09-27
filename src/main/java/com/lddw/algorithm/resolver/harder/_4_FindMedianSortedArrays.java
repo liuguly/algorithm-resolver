4d6d5cf53e61c5c5bfc3fc86100a182f4d4592f1
liuxin <lx_java@foxmail.com>
2020-09-27 14:34:21 +0800

    commit

--- src/main/java/com/lddw/algorithm/resolver/harder/_4_FindMedianSortedArrays.java
+++ src/main/java/com/lddw/algorithm/resolver/harder/_4_FindMedianSortedArrays.java
@@ -36,8 +36,8 @@ public class _4_FindMedianSortedArrays {
         int c = (nums1.length + nums2.length) % 2;
         int index = 0;
         if (c == 1) {
-            //奇数
-            int midIndex = (nums1.length + nums2.length) / 2 ;
+            // 奇数
+            int midIndex = (nums1.length + nums2.length) / 2;
             while (n1Key < nums1.length || n2Key < nums2.length) {
                 index++;
                 int v1 = nums1[n1Key];
@@ -55,12 +55,11 @@ public class _4_FindMedianSortedArrays {
                 }
             }
         } else {
-            //奇数 9/2 = 4   中间位置是4    123456789
-            int midIndex = (nums1.length + nums2.length ) / 2 ;
-            //偶数 8/2 =4   中间位置是3和4    12345678
+            // 奇数 9/2 = 4 中间位置是4 123456789
+            int midIndex = (nums1.length + nums2.length) / 2;
+            // 偶数 8/2 =4 中间位置是3和4 12345678
             int mideIndex1 = (nums1.length + nums2.length) / 2;
             int midIndex2 = (nums1.length + nums2.length - 1) / 2;
-
 
         }
         return 0;
@@ -69,55 +68,54 @@ public class _4_FindMedianSortedArrays {
     public static double findMedianSortedArrays1(int[] nums1, int[] nums2) {
         // 使nums1成为较短数组,不仅可以提高检索速度,同时可以避免一些边界问题
         if (nums1.length > nums2.length) {
-          int[] temp = nums1;
-          nums1 = nums2;
-          nums2 = temp;
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
         }
-    
+
         int len1 = nums1.length;
         int len2 = nums2.length;
-        int leftLen = (len1 + len2 + 1) / 2; //两数组合并&排序后,左半边的长度
-        
+        int leftLen = (len1 + len2 + 1) / 2; // 两数组合并&排序后,左半边的长度
+
         // 对数组1进行二分检索
         int start = 0;
         int end = len1;
         while (start <= end) {
-          // 两个数组的被测数A,B的位置(从1开始计算)
-          // count1 = 2 表示 num1 数组的第2个数字
-          // 比index大1
-          int count1 = start + ((end - start) / 2);
-          int count2 = leftLen - count1;
-          
-          if (count1 > 0 && nums1[count1 - 1] > nums2[count2]) {
-            // A比B的next还要大
-            end = count1 - 1;
-          } else if (count1 < len1 && nums2[count2 - 1] > nums1[count1]) {
-            // B比A的next还要大
-            start = count1 + 1;
-          } else {
-            // 获取中位数
-            int result =  (count1 == 0)? nums2[count2 - 1]: // 当num1数组的数都在总数组右边
-                          (count2 == 0)? nums1[count1 - 1]: // 当num2数组的数都在总数组右边
-                          Math.max(nums1[count1 - 1], nums2[count2 - 1]); // 比较A,B
-            if (isOdd(len1 + len2)) {
-              return result;
+            // 两个数组的被测数A,B的位置(从1开始计算)
+            // count1 = 2 表示 num1 数组的第2个数字
+            // 比index大1
+            int count1 = start + ((end - start) / 2);
+            int count2 = leftLen - count1;
+
+            if (count1 > 0 && nums1[count1 - 1] > nums2[count2]) {
+                // A比B的next还要大
+                end = count1 - 1;
+            } else if (count1 < len1 && nums2[count2 - 1] > nums1[count1]) {
+                // B比A的next还要大
+                start = count1 + 1;
+            } else {
+                // 获取中位数
+                int result = (count1 == 0) ? nums2[count2 - 1] : // 当num1数组的数都在总数组右边
+                        (count2 == 0) ? nums1[count1 - 1] : // 当num2数组的数都在总数组右边
+                                Math.max(nums1[count1 - 1], nums2[count2 - 1]); // 比较A,B
+                if (isOdd(len1 + len2)) {
+                    return result;
+                }
+
+                // 处理偶数个数的情况
+                int nextValue = (count1 == len1) ? nums2[count2]
+                        : (count2 == len2) ? nums1[count1] : Math.min(nums1[count1], nums2[count2]);
+                return (result + nextValue) / 2.0;
             }
-    
-            // 处理偶数个数的情况
-            int nextValue = (count1 == len1) ? nums2[count2]:
-                            (count2 == len2) ? nums1[count1]:
-                            Math.min(nums1[count1], nums2[count2]);
-            return (result + nextValue) / 2.0;
-          }
         }
-    
+
         return Integer.MIN_VALUE; // 绝对到不了这里
-      }
-    
-      // 奇数返回true,偶数返回false
-      private static boolean isOdd(int x) {
+    }
+
+    // 奇数返回true,偶数返回false
+    private static boolean isOdd(int x) {
         return (x & 1) == 1;
-      }
+    }
 
     public static void main(String[] args) {
         int[] nums1 = { 3, 4, 7, 9 };
@@ -125,8 +123,7 @@ public class _4_FindMedianSortedArrays {
         double result = findMedianSortedArrays1(nums1, nums2);
         System.out.println(result);
 
-
-        int[] nums3 = { 7,11,15,16,20};
+        int[] nums3 = { 7, 11, 15, 16, 20 };
         int[] nums4 = { 2, 5, 7, 10, 14, 17 };
         System.out.println("nums3.len=" + nums3.length);
         System.out.println(findMedianSortedArrays1(nums3, nums4));
---
+++ src/main/java/com/lddw/algorithm/resolver/medium/_4_LongestPalindrome.java
@@ -0,0 +1,11 @@
+package com.lddw.algorithm.resolver.medium;
+
+public class _4_LongestPalindrome {
+
+    public String longestPalindrome(String s) {
+
+        return "";
+    }
+
+
+}
