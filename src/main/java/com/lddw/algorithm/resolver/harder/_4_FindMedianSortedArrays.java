4ae701a5b7def8fa5e51f4e24a9c5c16d7f28a26
liuxin <lx_java@foxmail.com>
2020-09-27 14:24:06 +0800

    commit

--- src/main/java/com/lddw/algorithm/resolver/harder/_4_FindMedianSortedArrays.java
+++ src/main/java/com/lddw/algorithm/resolver/harder/_4_FindMedianSortedArrays.java
@@ -1,13 +1,22 @@
 package com.lddw.algorithm.resolver.harder;
 
-
 /**
  * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为
  * O(log(m + n))。 你可以假设 nums1 和 nums2 不会同时为空。
  * 
- * 示例 1: nums1 = [1, 3] nums2 = [2] 则中位数是 2.0 
- * 示例 2: nums1 = [1,2,4, 6, 9] nums2 = [3，6，9] 则中位数是 5.0 
- * 示例 3: nums1 = [1, 2] nums2 = [3, 4,5,6] 则中位数是 (3 + 4)/2 = 3.5
+ * 示例 1: nums1 = [1, 3] nums2 = [2] 则中位数是 2.0 示例 2: nums1 = [1,2,4, 6, 9] nums2
+ * = [3，6，9] 则中位数是 5.0 示例 3: nums1 = [1, 2] nums2 = [3, 4,5,6] 则中位数是 (3 + 4)/2 =
+ * 3.5
+ * 
+ * 
+ * 方法一：二分查找
+ * 
+ * 给定两个有序数组，要求找到两个有序数组的中位数，最直观的思路有以下两种：
+ * 
+ * 1:使用归并的方式，合并两个有序数组，得到一个大的有序数组。大的有序数组的中间位置的元素，即为中位数。
+ * 2:不需要合并两个有序数组，只要找到中位数的位置即可。由于两个数组的长度已知，因此中位数对应的两个数组的下标之和也是已知的。维护两个指针，初始时分别指向两个数组的下标
+ * 0的位置，每次将指向较小值的指针后移一位（如果一个指针已经到达数组末尾，则只需要移动另一个数组的指针），直到到达中位数的位置。
+ * 
  * 
  * 
  * 来源：力扣（LeetCode）
@@ -18,20 +27,109 @@ package com.lddw.algorithm.resolver.harder;
  */
 public class _4_FindMedianSortedArrays {
 
-
     public static double findMedio(int[] nums1, int[] nums2) {
-        // int[] nums1 = { 3, 4, 7, 9};
-        // int[] nums2 = { 2, 5, 7, 10 };
-        // (m+n)/2   (m+n+2)/2 
+        // int[] nums1 = { 3, 4, 7, 9,11,12};
+        // int[] nums2 = { 2, 5, 7, 10, 14 };
+        // (m+n)/2 (m+n+2)/2
+        int n1Key = 0;
+        int n2Key = 0;
+        int c = (nums1.length + nums2.length) % 2;
+        int index = 0;
+        if (c == 1) {
+            //奇数
+            int midIndex = (nums1.length + nums2.length) / 2 ;
+            while (n1Key < nums1.length || n2Key < nums2.length) {
+                index++;
+                int v1 = nums1[n1Key];
+                int v2 = nums2[n2Key];
+                if (v1 < v2) {
+                    if (midIndex == index) {
+                        return nums1[n1Key];
+                    }
+                    n1Key++;
+                } else {
+                    if (midIndex == index) {
+                        return nums2[n2Key];
+                    }
+                    n2Key++;
+                }
+            }
+        } else {
+            //奇数 9/2 = 4   中间位置是4    123456789
+            int midIndex = (nums1.length + nums2.length ) / 2 ;
+            //偶数 8/2 =4   中间位置是3和4    12345678
+            int mideIndex1 = (nums1.length + nums2.length) / 2;
+            int midIndex2 = (nums1.length + nums2.length - 1) / 2;
+
+
+        }
         return 0;
     }
 
+    public static double findMedianSortedArrays1(int[] nums1, int[] nums2) {
+        // 使nums1成为较短数组,不仅可以提高检索速度,同时可以避免一些边界问题
+        if (nums1.length > nums2.length) {
+          int[] temp = nums1;
+          nums1 = nums2;
+          nums2 = temp;
+        }
+    
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int leftLen = (len1 + len2 + 1) / 2; //两数组合并&排序后,左半边的长度
+        
+        // 对数组1进行二分检索
+        int start = 0;
+        int end = len1;
+        while (start <= end) {
+          // 两个数组的被测数A,B的位置(从1开始计算)
+          // count1 = 2 表示 num1 数组的第2个数字
+          // 比index大1
+          int count1 = start + ((end - start) / 2);
+          int count2 = leftLen - count1;
+          
+          if (count1 > 0 && nums1[count1 - 1] > nums2[count2]) {
+            // A比B的next还要大
+            end = count1 - 1;
+          } else if (count1 < len1 && nums2[count2 - 1] > nums1[count1]) {
+            // B比A的next还要大
+            start = count1 + 1;
+          } else {
+            // 获取中位数
+            int result =  (count1 == 0)? nums2[count2 - 1]: // 当num1数组的数都在总数组右边
+                          (count2 == 0)? nums1[count1 - 1]: // 当num2数组的数都在总数组右边
+                          Math.max(nums1[count1 - 1], nums2[count2 - 1]); // 比较A,B
+            if (isOdd(len1 + len2)) {
+              return result;
+            }
+    
+            // 处理偶数个数的情况
+            int nextValue = (count1 == len1) ? nums2[count2]:
+                            (count2 == len2) ? nums1[count1]:
+                            Math.min(nums1[count1], nums2[count2]);
+            return (result + nextValue) / 2.0;
+          }
+        }
+    
+        return Integer.MIN_VALUE; // 绝对到不了这里
+      }
+    
+      // 奇数返回true,偶数返回false
+      private static boolean isOdd(int x) {
+        return (x & 1) == 1;
+      }
 
     public static void main(String[] args) {
-        int[] nums1 = { 3,4,7,9};
+        int[] nums1 = { 3, 4, 7, 9 };
         int[] nums2 = { 2, 5, 7, 10 };
-        double result = findMedianSortedArrays(nums1, nums2);
+        double result = findMedianSortedArrays1(nums1, nums2);
         System.out.println(result);
+
+
+        int[] nums3 = { 7,11,15,16,20};
+        int[] nums4 = { 2, 5, 7, 10, 14, 17 };
+        System.out.println("nums3.len=" + nums3.length);
+        System.out.println(findMedianSortedArrays1(nums3, nums4));
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
